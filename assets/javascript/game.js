24b9daa4f4d246a59f178a1134597b7b99475404
David Hauck <dhauck@mac.com>
2019-10-13 13:53:01 -0400

    final version with cleaned up html and new rules animation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
 
 // Create variables for random number, crystal values, wins, losses
-// let randomNumber = 0;
-// let crystal1 = 0;
-// let crystal2 = 0;
-// let crystal3 = 0;
-// let crystal4 = 0;
 let wins = 0;
 let losses = 0;
 let guessTotal = 0;
 let yourTotal = 0;
 
-$("#your-total").html(yourTotal);
+// set intial html
 $("#wins").html("Wins: " + wins);
 $("#losses").html("Losses: " + losses);
 
+// auto replay
 function playAgain() {
     randomNumber = "";
     guessTotal = 0;
@@ -25,32 +21,21 @@ function playAgain() {
     $( ".crystal-image" ).empty();
     $( ".crystals" ).empty();
     $('#random-number').html(randomNumber);
-    $('#your-total').html(yourTotal);
+    $('#your-total').html(randomNumber);
  }
 
 // Generate random number between 19 - 120, put it in the div
 let randomNumber = Math.floor(Math.random() * 102 + 19);
 randomNumber = randomNumber*1;
 $('#random-number').html(randomNumber);
+$("#your-total").html(randomNumber);
 
-// Generate random number for each crystal between 1 - 12
+// Rules button
+$("button").click(function() {
+    $( "p" ).slideToggle(); 
+});
 
-// for (let i = 0; i < 4; i++) {
-//     let b = $("<button>")
-//     let crystalNumber = Math.floor(Math.random() * 12 + 1);
-//     b.attr("data-random", crystalNumber);
-//     b.addClass("btn number")
-//     b.text(crystalNumber);
-//     $('#crystals').append(b);
-//     console.log(b);
-// }
-
-$("button").click(function(){
-    $("p").toggle(500);
-  });
-
-
-
+// set images
 function newImages() {
 for (let i = 0; i < 4; i++) {
     let imageCrystal = $("<img>");
@@ -62,75 +47,44 @@ for (let i = 0; i < 4; i++) {
     $("#crystals").append(imageCrystal);
 }
 
-// debugger;
-
-
-$(".crystal-image").click(function() {
-    let crystalValue = parseInt($(this).attr("data-crystalValue"));
-    $( "#crystalID" + $(this).attr("data-location") ).animate({
-        padding: "30px"
-      }, 300 ); 
-      $( "#crystalID" + $(this).attr("data-location") ).animate({
-        padding: "10px"
-      }, 200 );
-      $( "#crystalID" + $(this).attr("data-location") ).animate({
-        padding: "20px"
-      }, 100 );
-    guessTotal += crystalValue;
-    $("#your-total").html(guessTotal);
- 
-    checkWin();
-});
+// image click logic
+    $(".crystal-image").click(function() {
+        let crystalValue = parseInt($(this).attr("data-crystalValue"));
+        $( "#crystalID" + $(this).attr("data-location") ).animate({
+            padding: "30px"
+        }, 300 ); 
+        $( "#crystalID" + $(this).attr("data-location") ).animate({
+            padding: "10px"
+        }, 200 );
+        $( "#crystalID" + $(this).attr("data-location") ).animate({
+            padding: "20px"
+        }, 100 );
+        randomNumber -= crystalValue;
+        $("#your-total").html(randomNumber);
+    
+        checkWin();
+    });
 }
 
-
-// for (let i = 0; i < 4; i++) {
-//     let b = $("<button>")
-//     let crystalNumber = Math.floor(Math.random() * 12 + 1);
-//     b.attr("data-random", crystalNumber);
-//     b.addClass("btn number")
-//     b.text(crystalNumber);
-//     $('#crystals').append(b);
-//     console.log(b);
-// }
-
-// $(".number").click(function() {
-//     const val = parseInt($(this).attr("data-random"));
-//     score += val;
-//     checkWin();
-//     console.log($(this).attr("data-random"));
-// });
-
-
+// check win-loss conditions
 function checkWin() {
 
-    if (guessTotal > randomNumber) {
-        // $("#score, #your-total").empty();
+    if (randomNumber < 0) {
         losses += 1;
         $("#losses").html("Losses: " + losses);
         playAgain();
         newImages();
     }
     
-    else if (guessTotal === randomNumber) {
+    else if (randomNumber === 0) {
         wins += 1;
         $("#wins").html("Wins: " + wins);
-        // $("#score, #your-total, #crystals").empty();
         playAgain();
         newImages();
     }
 }
-
-// $(".clear").on("click", function() {
-//     // call initialize to reset app
-//     playAgain();
-//   });
-  
-//   playAgain();
   
 newImages();
-
-
 
 });
 
